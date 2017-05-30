69bfa5bef8b2ee44ef60fc4308f476d2455661ca
Kleber Almeida Toyota <klebertiko@gmail.com>
2017-05-30 18:34:07 -0300

    Criado metodo get recebendo ou nao um query parameter nome.

--- python-api.py
+++ python-api.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, Response, json, make_response
+from flask import Flask, Response, make_response, jsonify, request
 
 app = Flask(__name__)
 
 
 @app.route('/ola', methods=['GET'])
 def ola():
-    data = {
-        'message': 'Olá Mundo'
-    }
-    return Response(json.dumps(data),
-                    status=200,
-                    content_type='application/json; charset=utf-8')
-
+    if 'nome' in request.args:
+        data = {'message': 'Olá ' + request.args['nome']}
+        response = jsonify(data)
+        response.status_code = 200
+        response.content_type = 'application/json; charset=utf-8'
+        return response
+    else:
+        data = {'message': 'Olá Mundo'}
+        response = jsonify(data)
+        response.status_code = 200
+        response.content_type = 'application/json; charset=utf-8'
+        return response
 
 if __name__ == '__main__':
     app.run()
